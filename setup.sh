7d11fdf0d719bae95e206152f599c5908096fe57
sizukutamago <sizukutamago@gmail.com>
2023-10-14 13:08:32 +0900

    i

---
+++ setup.sh
@@ -0,0 +1,10 @@
+brew install --cask google-japanese-ime
+brew install --cask google-chrome
+brew install --cask slack
+brew install --cask discord
+brew install --cask warp
+brew install --cask notion
+brew install --cask visual-studio-code
+
+brew install mas
+mas install 1176895641 # Spark
