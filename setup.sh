1210d57a7e17f2e4e071272bed1a38343798cba5
sizukutamago <sizukutamago@gmail.com>
2023-10-14 13:10:47 +0900

    add 1pass

--- setup.sh
+++ setup.sh
@@ -5,6 +5,7 @@ brew install --cask discord
 brew install --cask warp
 brew install --cask notion
 brew install --cask visual-studio-code
+brew install --cask 1password
 
 brew install mas
 mas install 1176895641 # Spark
